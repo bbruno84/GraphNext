403b03702bf1bebf165aff11c6042bcb6f4f5135
Bruno <valerio.buriani@gmail.com>
2025-07-13 10:32:08 +0200

    Initial project structure for GraphNext

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let package = Package(
+    name: "GraphNext",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(name: "GraphNext", targets: ["GraphNext"]),
+        .library(name: "GraphPersistence", targets: ["GraphPersistence"]),
+        .library(name: "GraphSyncEngine", targets: ["GraphSyncEngine"]),
+        .library(name: "GraphAssets", targets: ["GraphAssets"])
+    ],
+    targets: [
+        .target(name: "GraphNext"),
+        .target(name: "GraphPersistence"),
+        .target(name: "GraphSyncEngine"),
+        .target(name: "GraphAssets"),
+        .testTarget(name: "GraphNextTests", dependencies: ["GraphNext"]),
+        .testTarget(name: "GraphPersistenceTests", dependencies: ["GraphPersistence"]),
+        .testTarget(name: "GraphSyncEngineTests", dependencies: ["GraphSyncEngine"]),
+        .testTarget(name: "GraphAssetsTests", dependencies: ["GraphAssets"])
+    ]
+)
+
